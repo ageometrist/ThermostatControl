82a6c0376827e0708051e6bedbfb920d1d35c742
ageometrist <schnoodleparent@gmail.com>
2025-10-15 22:15:20 -0400

    added Debug

--- main.py
+++ main.py
@@ -4,30 +4,44 @@ import os
 import time
 import pytz
 import gpiozero
+import datetime
+
+debug = False
 
 def initialize_Sensors():
     # BME280 sensor address (default address)
     address = 0x76
 
-    # Initialize I2C bus
-    bus = smbus2.SMBus(1)
+    if debug:
+        bus = 1
+        calibration_params = None
+    else:
+        # Initialize I2C bus
+        bus = smbus2.SMBus(1)
 
-    # Load calibration parameters
-    calibration_params = bme280.load_calibration_params(bus, address)
+        # Load calibration parameters
+        calibration_params = bme280.load_calibration_params(bus, address)
     return bus, address, calibration_params
 
 def Read_Sensors(bus, address, calibration_params):
-    # Read sensor data
-    data = bme280.sample(bus, address, calibration_params)
-
-    # Extract temperature, pressure, humidity, and corresponding timestamp
-    temperature_celsius = data.temperature
-    humidity = data.humidity
-    pressure = data.pressure
-    timestamp = data.timestamp
     # Adjust timezone
     # Define the timezone you want to use (list of timezones: https://gist.github.com/mjrulesamrat/0c1f7de951d3c508fb3a20b4b0b33a98)
     desired_timezone = pytz.timezone('America/New_York')  # Replace with your desired timezone
+    
+    if debug:
+        temperature_celsius = 25.0
+        humidity = 50.0
+        pressure = 1013.25
+        timestamp = datetime.datetime.utcnow()
+    else:
+        # Read sensor data
+        data = bme280.sample(bus, address, calibration_params)
+
+        # Extract temperature, pressure, humidity, and corresponding timestamp
+        temperature_celsius = data.temperature
+        humidity = data.humidity
+        pressure = data.pressure
+        timestamp = data.timestamp
 
     # Convert the datetime to the desired timezone
     timestamp = timestamp.replace(tzinfo=pytz.utc).astimezone(desired_timezone)
